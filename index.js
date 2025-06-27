d95cddf2d6bef87993f6a5dd56f72df10b961dc4
mohammed0982 <simmoha08@gmail.com>
2025-06-27 02:37:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, LocalAuth } = require('whatsapp-web.js'); const 
+qrcode = require('qrcode-terminal'); const client = new Client({
+    authStrategy: new LocalAuth(), puppeteer: { headless: true, 
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+client.on('qr', qr => { qrcode.generate(qr, { small: true });
+});
+client.on('ready', () => { console.log('Client is ready!');
+});
+client.on('message', message => { if (message.body === '!ping') { 
+        message.reply('pong');
+    }
+});
+client.initialize();
