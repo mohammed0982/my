1642f04857509f2d5c29755b3e150cf047bdb747
mohammed0982 <simmoha08@gmail.com>
2025-06-27 03:51:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { Client, LocalAuth } = require('whatsapp-web.js'); const 
-qrcode = require('qrcode-terminal'); const client = new Client({
-    authStrategy: new LocalAuth(), puppeteer: { headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
+qrcode = require('qrcode-terminal'); 
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { 
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage'
+    ],
+    executablePath: process.env.CHROMIUM_PATH || '/usr/bin/chromium'
+  }
 });
 client.on('qr', qr => { qrcode.generate(qr, { small: true });
 });
